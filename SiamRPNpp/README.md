e165768475046e95203f27c4324ffc10e64aa743
qcrs <89289383+qcrs@users.noreply.github.com>
2025-11-14 15:37:21 +0800

    Delete SiamRPNpp directory

--- SiamRPNpp/README.md
+++
@@ -1,2 +0,0 @@
-
-[哔哩哔哩](https://www.bilibili.com/video/BV1z54y1r7HJ#reply3724917590)
--- SiamRPNpp/SiamRPNpp/bin/cmd_eval.sh
+++
@@ -1,6 +0,0 @@
-python ./bin/eval.py \
---tracker_path ./hp_search_result \
---dataset VOT2018 \
---num 4 \
---tracker_name  'checkpoint_e41*' #'checkpoin*' 
-
